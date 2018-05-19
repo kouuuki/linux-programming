ab2ccd72d93b456f057eb079e6a37d01d4d87985
kouuuki <mark.web.888@gmail.com>
2018-05-19 21:18:12 +0900

    new c file

---
+++ cat.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+static void do_cat(const char *path);
+static void die(const char *s);
+
+int
+main(int argc, char *argv[])
---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void num(void) {
+  printf("Hello\n");
+}
+
+int main(void) {
+  num();
+}
+
+
