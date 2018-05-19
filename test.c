c9bdac454067b6887032c956c6e1785706195e10
kouuuki <mark.web.888@gmail.com>
2018-05-19 21:29:08 +0900

    fizzbuzz

--- test.c
+++ test.c
@@ -4,8 +4,25 @@ void num(void) {
   printf("Hello\n");
 }
 
-int main(void) {
-  num();
+int fizzbuzz(void) {
+  for (int i = 0; i <= 100; i++) {
+    if (i % 5 == 0) {
+      printf("Fizz\n");
+    } else if (i % 3 == 0) {
+      printf("Buzz\n");
+    } else {
+      printf("%i\n", i);
+    }
+  }
+  return 0;
 }
 
-
+int main(void) {
+  // num();
+  fizzbuzz();
+  // int a = 0;
+  // while (a < 10) {
+  //   printf("%d\n", a);
+  //   a++;
+  // }
+}
