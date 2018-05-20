8a6810d5e3c4f6c8acf15275415d2673d2b8088a
kouuuki <mark.web.888@gmail.com>
2018-05-20 13:21:01 +0900

    scan

--- scan.c
+++ scan.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 
+int hand();
+int sw();
+int sum();
+
+int main(void) {
+  // // int data;
+  // // scanf("%d", &data);
+  // // printf("%d\n", data);
+  // // hand();
+  // // sw();
+  // sum(90, 30);
+  char c  = 'A';
+  printf("%c\n", c);
+  return 0;
+}
+
+
 int hand(void) {
   for (int i = 0; i <= 100; i++) {
     printf("%d\n", i);
@@ -7,11 +24,24 @@ int hand(void) {
   return 0;
 }
 
+int sw(void) {
+  int i = 10;
 
-int main(void) {
-  int data;
-  scanf("%d", &data);
-  printf("%d\n", data);
-  hand();
+  switch (i) {
+    case 1:
+      printf("Hello\n");
+      break;
+    case 10:
+      printf("World\n");
+      break;
+  }
+  return 0;
+}
+
+// sum
+
+int sum(int a, int b){
+  int sum = a * b;
+  printf("%d\n", sum);
   return 0;
 }
